63e9f958622c9f4a4e5c9d4136dc662009913635
Dark-Abyssal <yikaiclarence@gmail.com>
2024-07-15 13:21:31 +0800

    Update Project Snake.py

--- Project Snake.py
+++ Project Snake.py
@@ -4,6 +4,8 @@ import turtle
 import time
 import random
 delay = 0.1
+score = 0
+high_score = 0
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor('black')
@@ -12,7 +14,7 @@ wn.tracer(0)
 head = turtle.Turtle()
 head.speed(0)
 head.shape("circle")
-head.color("white")
+head.color("green")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
@@ -30,6 +32,7 @@ pen.color("green")
 pen.penup()
 pen.hideturtle()
 pen.goto(0,260)
+pen.write("Score:0 High score:0", align = "center", font=("Courier", 24, "normal"))
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -56,10 +59,10 @@ def move():
         x = head.xcor()
         head.setx(x+20)
 wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
+wn.onkeypress(go_up, "y")
+wn.onkeypress(go_down, "h")
+wn.onkeypress(go_left, "g")
+wn.onkeypress(go_right, "j")
 while True:
     wn.update()
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
@@ -69,6 +72,7 @@ while True:
         for segment in segments:
             segment.goto(1000,1000)
         segments.clear()
+        score = 0
         delay = 0.1
         pen.clear()
         pen.write("Score: {} High score: {}".format(score, high_score),align="center", font=("Courier", 24, "normal"))
@@ -83,7 +87,11 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
         delay -= 0.001
+        score += 10
+        if score > high_score:
+            high_score = score
         pen.clear()
+        pen.write("Score: {} High score: {}".format(score,high_score), align="center", font=("Courier", 24, "normal"))
     for index in range(len(segments)-1,0,-1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
@@ -101,8 +109,9 @@ while True:
             for segment in segments:
                 segment.goto(1000,1000)
             segments.clear()
+            score = 0
             delay = 0.1
             pen.clear()
-            
+            pen.write("Score: {} High score: {}".format(score,high_score), align="center", font=("Courier", 24, "normal"))
     time.sleep(delay)
 wn.mainloop()
