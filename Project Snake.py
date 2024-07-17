dabe8eaa84e411568edd86f7711fba271e907893
Dark-Abyssal <yikaiclarence@gmail.com>
2024-07-17 16:25:01 +0800

    Update Project Snake.py

--- Project Snake.py
+++ Project Snake.py
@@ -11,6 +11,7 @@ wn.title("Snake Game")
 wn.bgcolor('black')
 wn.setup(width=600, height=600)
 wn.tracer(0)
+#Snake head set up
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -18,6 +19,7 @@ head.color("white")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+#Food set up
 food = turtle.Turtle()
 food.speed(0)
 food.shape("square")
@@ -25,14 +27,16 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 segments = []
+#Score display set up
 pen = turtle.Turtle()
 pen.speed(0)
-pen.shape("circle")
+pen.shape("square")
 pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0,260)
 pen.write("Score:0 High score:0", align = "center", font=("Courier", 24, "normal"))
+#Movement functions
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -45,6 +49,7 @@ def go_left():
 def go_right():
     if head.direction != "left":
         head.direction = "right"
+#movement logic
 def move():
     if head.direction == "up":
         y = head.ycor()
@@ -58,13 +63,16 @@ def move():
     if head.direction == "right":
         x = head.xcor()
         head.setx(x+20)
+#Key bindings
 wn.listen()
 wn.onkeypress(go_up, "Up")
 wn.onkeypress(go_down, "Down")
 wn.onkeypress(go_left, "Left")
 wn.onkeypress(go_right, "Right")
+#Main Game Loop
 while True:
     wn.update()
+    #Collision with borders
     if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
         time.sleep(1)
         head.goto(0,0)
@@ -76,6 +84,7 @@ while True:
         delay = 0.1
         pen.clear()
         pen.write("Score: {} High score: {}".format(score, high_score),align="center", font=("Courier", 24, "normal"))
+        #Collision with food
     if head.distance(food) <20:
         x = random.randint(-290,290)
         y = random.randint(-290,290)
@@ -92,6 +101,7 @@ while True:
             high_score = score
         pen.clear()
         pen.write("Score: {} High score: {}".format(score,high_score), align="center", font=("Courier", 24, "normal"))
+        #Move segments
     for index in range(len(segments)-1,0,-1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
@@ -101,6 +111,7 @@ while True:
         y = head.ycor()
         segments[0].goto(x,y)
     move()
+    #Self-collision check
     for segment in segments:
         if segment.distance(head)<20:
             time.sleep(1)
@@ -113,5 +124,5 @@ while True:
             delay = 0.1
             pen.clear()
             pen.write("Score: {} High score: {}".format(score,high_score), align="center", font=("Courier", 24, "normal"))
-    time.sleep(delay)
+    time.sleep(delay) #Delay management
 wn.mainloop()
